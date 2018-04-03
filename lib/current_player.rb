0229237bc8cbea076b62f5972c0d8a5d0dc427de
Brent Bauman <brentbauman86@gmail.com>
2018-04-03 22:21:22 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,12 @@ def turn_count(board)
   count = 0
     board.each do |move|
     end
+
       if move == "X" || move == "O"
         count += 1
-
+        
       end
-    end
+    
 
 
 def current_player(board)
