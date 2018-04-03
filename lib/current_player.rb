40c1b57157162e2d6f0d0ab6c7d52923305963a0
Brent Bauman <brentbauman86@gmail.com>
2018-04-03 21:42:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,9 +7,8 @@ def turn_count(board)
     end
 
 def current_player(board)
-  if turn_count(board).select {|num| num.even?} == "X" || turn_count(board).select {|num| num.odd?} == "O"
+  if turn_count(board).even? == "X" || turn_count(board).odd? == "O"
 
-
-end
-end
+    end
+  end
 end
